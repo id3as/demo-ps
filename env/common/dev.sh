63b98120ad8dfc5e226f6b388a1652a5e9ffece8
robashton <robashton@codeofrob.com>
2020-07-02 10:14:26 +0100

    shell.nix to root

--- env/common/dev.sh
+++
@@ -1,8 +0,0 @@
-# History is nice in shells, let's have that
-export ERL_AFLAGS="-kernel shell_history enabled"
-
-# EDTS needs to know where erlexec is
-export PATH=$PATH:$(dirname "$(which erl)")/../lib/erlang/erts-10.4/bin/
-
-# Use nix set-up
-use_nix
